7f86cff3843d618d0569159db0ba76d91c90e9a2
vihoangson <vihoangson@gmail.com>
2019-04-27 04:25:02 +0700

    first commit

---
+++ com.js
@@ -0,0 +1,265 @@
+var romanji = [];
+var hiragana = []
+var katakana = [];
+
+var arrConcat = [];
+
+var min = 0;
+var max = 0;
+
+var currentKey = '';
+var currentArray = [];
+
+var result_default = '<span style="font-size:11px">click Ä‘á»ƒ xem Ä‘Ă¡p Ă¡n </span>';
+
+
+var getData = function(){
+    var url = 'data.json';
+    // var url = 'http://localhost/study_japanese/data.php';
+    $.ajax({
+        url:url,
+        type:'GET',
+        dataType:'json',
+        beforeSend:function(){
+            $('#loading').show();
+        },
+        complete:function(){
+            $('#loading').hide();
+            init();
+        },
+        success:function(data){
+
+            //console.log(data)
+
+            if(  data.romanji != undefined){
+                romanji = data.romanji
+            }
+            else{
+                alert('not found data');
+                $('#practice').hide();
+                $('#setup-range').hide();
+            }
+
+            if(data.katakana != undefined){
+                katakana = data.katakana
+            }
+
+            if( data.hiragana != undefined){
+                hiragana = data.hiragana
+            }
+            // console.log(katakana);
+
+        }
+    })
+}
+
+var init = function(){
+    arrConcat = hiragana.concat(katakana);
+    $('#input-from').val(getCookie('inputFrom'));
+    $('#input-to').val(getCookie('inputTo'))
+    prevList();
+    $('#setup-range').show();
+}
+
+var startPratice = function(){
+    resetPracice();
+    $('#practice').show();
+    $('#setup-range').hide();
+    listChar();
+    random_char();
+}
+
+var resetPracice = function(){
+    $('#result-typing').html('');
+    $('#show-result').html(result_default);
+    $('#show-result').removeClass('bg-success bg-danger')
+    $('.group-typing').show();
+    $('#typing').focus();
+    $('#message-result').text('');
+}
+
+var resetData = function(){
+    var inputFrom = parseInt($('#input-from').val());
+    var inputTo = parseInt($('#input-to').val());
+    // console.log(arrConcat.length)
+    currentArray = arrConcat.slice(inputFrom-1,inputTo)
+    max = currentArray;
+    // console.log(currentArray)
+    startPratice();
+
+
+}
+
+
+
+var check_char = function(x){
+    var input_value = $('#typing').val().trim().toLowerCase();
+    if(input_value == ''){
+        showMessage('danger','HĂ£y gĂµ chá»¯ Ä‘i nĂ o !');
+        return;
+    }
+
+    var class_status = '';
+    var romanji_char = currentArray[currentKey][0]
+    var char_type= currentArray[currentKey][2];
+    loadResult()
+
+    if(input_value == romanji_char){
+        $('#message-result').html('<i class="far fa-grin-beam"></i> : '+input_value);
+        class_status = 'bg-success';
+        currentArray.splice(currentKey,1)
+        max = currentArray.length - 1;
+        //console.log(max)
+
+    }
+    else{
+        $('#message-result').html('<i class="far fa-frown"></i> : '+input_value);
+        class_status = 'bg-danger';
+
+    }
+
+    $('#show-result').addClass(class_status);
+
+    $('#list-char li.char-'+char_type+romanji_char).addClass('active').addClass(class_status)
+
+
+    //console.log($('#list-char').find('li.katakana-'+romanji[currentKey]))
+
+    $('#typing').val('');
+    $('#typing').focus();
+    if(max < 0){
+        $('.group-typing').hide();
+        $('#btn-reset').focus()
+        showMessage('success','Báº¡n Ä‘Ă£ hoĂ n thĂ nh bĂ i táº­p ')
+    }
+}
+
+var loadResult = function(class_status){
+    var romanji_char = currentArray[currentKey][0]
+    var jp_char = currentArray[currentKey][1];
+
+    $('#show-result').html( jp_char+' = '+romanji_char );
+    $('#show-result').removeClass('bg-success bg-danger')
+    showMessage();
+
+}
+
+var random_char = function(){
+    if(max > 0){
+        currentKey = Math.floor(Math.random() * (max - min) + min);
+    }
+    else{
+        currentKey = 0;
+    }
+    //alert(currentKey)
+
+    $('#show-char').text(currentArray[currentKey][1]);
+    console.log(max)
+    console.log(currentKey)
+}
+
+var listChar = function(){
+    var html = '';
+    for(var i=0;i< currentArray.length;i++){
+        html+='<li class="char-'+currentArray[i][2]+currentArray[i][0]+'">'+currentArray[i][1]+'<br/><span class="romanji">'+currentArray[i][0]+'</span>'+'</li>';
+    }
+    $('#list-char').html(html)
+}
+
+var prevList = function(){
+    var html = '';
+    for(var i=0;i< hiragana.length;i++){
+        html+='<li class="active" >'+hiragana[i][1]+'<br/>'+hiragana[i][0]+'<br/><span class="number">'+(i+1)+'</span>'+'</li>';
+    }
+    $('#prev-hiragana').html(html)
+
+    var html = '';
+    for(var i=0 ;i< katakana.length;i++){
+        html+='<li class="active" >'+katakana[i][1]+'<br/>'+katakana[i][0]+'<br/><span class="number">'+(i+1+hiragana.length)+'</span>'+'</li>';
+    }
+    $('#prev-katakana').html(html)
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+var showMessage = function(type,message){
+    if(!type){
+        $('#message').text('');
+        return;
+    }
+    $('#message').html('<div class="alert alert-'+type+'">'+message+'</div>');
+}
+
+$(function(){
+    getData();
+
+    $('#typing-form').submit(function(e){
+        e.preventDefault()
+        check_char(this);
+        random_char();
+    })
+
+    $('#typing').keyup(function(e){
+        if(e.keyCode != 13){
+            $('#show-result').html(result_default);
+            $('#message-result').text('');
+            $('#show-result').removeClass('bg-success bg-danger')
+            showMessage();
+        }
+
+    })
+
+    $('#btn-reset').click(function(){
+        resetData();
+    });
+
+    $('#start').click(function(){
+        var inputFrom = parseInt($('#input-from').val());
+        var inputTo = parseInt($('#input-to').val());
+
+        if(!inputFrom || !inputTo){
+            alert('Vui lĂ²ng nháº­p sá»‘ !')
+            return;
+        }
+
+        if(inputTo > 142){
+            inputTo =  142;
+        }
+        setCookie('inputFrom',inputFrom);
+        setCookie('inputTo',inputTo);
+        // console.log(arrConcat.length)
+        currentArray = arrConcat.slice(inputFrom-1,inputTo)
+        max = currentArray.length;
+        // console.log(currentArray)
+        startPratice();
+    })
+
+    $('#setup').click(function(){
+        $('body').remove();
+        window.location.reload();
+    })
+
+
+    $('#show-result').click(function(){
+        loadResult();
+    })
+    
+})
---
+++ index.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Japanese studing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <script
+            src="https://code.jquery.com/jquery-3.4.0.min.js"
+            integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg="
+            crossorigin="anonymous"></script>
+
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script src="com.js"></script>
+    <link rel="stylesheet" href="http://study-japanese.vinaweber.com/style.css">
+</head>
+<body>
+
+<div id="loading" class="margin-top-20 text text-danger">Đang tải data ...</div>
+
+<header>
+
+</header>
+<section class="container setup-range" id="setup-range">
+    <div class="row">
+        <h3 class="text-center">Vui lòng chọn dãy ký tự muốn thực hành</h3>
+        <div class="col-sm-12">
+            <h4 class="text-center">Hiragana</h4>
+            <ul class="list margin-top-20 list-char" id="prev-hiragana">
+
+            </ul>
+            <h4 class="text-center">Katakana</h4>
+            <ul class="list margin-top-20 list-char" id="prev-katakana">
+
+            </ul>
+        </div>
+
+        <div class="col-sm-12">
+            <label class="col-sm-2 text-right">Chọn từ số : </label>
+            <div class="col-sm-1 form-group">
+                <input value="1" id="input-from" type="number" class="form-control"/>
+            </div>
+            <label class="col-sm-1 text-right">đến số : </label>
+            <div class="col-sm-1  form-group">
+                <input value="10" id="input-to" type="number" maxlength="142" class="form-control"/>
+            </div>
+            <div class="col-sm-2">
+                <button id="start" class="btn btn-primary">Start</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="container practice" id="practice">
+    <div class="row">
+        <ul class="list margin-top-20 list-char" id="list-char">
+
+        </ul>
+
+    </div>
+    <div class="row">
+        <div class="col-sm-4 col-xs-6 col-sm-offset-2 result-header">
+            <h3> Nhập ký tự dưới</h3>
+        </div>
+        <div class="col-sm-4 col-xs-6 result-header">
+            <h3>Đáp án</h3>
+        </div>
+
+    </div>
+    <div class="row">
+
+        <div class="col-sm-4  col-xs-6 col-sm-offset-2 ">
+            <div class="result-style " id="show-char"></div>
+        </div>
+        <div class="col-sm-4  col-xs-6 result-relative">
+            <div class="result-style show-result" id="show-result"></div>
+            <div class="result-absolute"><span id="message-result"></span>  </div>
+        </div>
+
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <form id="typing-form" action="" autocomplete="off">
+                <div class="margin-top-20" id="message"></div>
+                <div class="form-group">
+                    <div class="group-typing">
+                        <label class="col-sm-3 col-xs-6 text-right">Nhập ký tự romanji</label>
+                        <div class="col-sm-3  col-xs-6 form-group">
+                            <input id="typing" class="form-control"/>
+                        </div>
+
+                        <div class="col-sm-2 col-xs-4 text-center clear-both-xs">
+                            <button class="btn btn-primary" id="check-char">Check</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-2  col-xs-4 text-center">
+                        <button class="btn btn-info" id="btn-reset">Làm lại</button>
+                    </div>
+                    <div class="col-sm-2  col-xs-4 text-center">
+                        <button class="btn btn-info" id="setup">Thiết lập</button>
+                    </div>
+
+
+                </div>
+
+
+            </form>
+        </div>
+    </div>
+
+</section>
+
+</body>
+</html>
